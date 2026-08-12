93ceca577f33a43933ddcdaa5b41c0186d095874
jdebilde <jean-jacques.debilde@student.uclouvain.be>
2026-08-12 17:43:31 +0200

    remove liveView

--- liveView/rot.m
+++
@@ -1,5 +0,0 @@
-function pR = rot(p, q)
-    R = q2DCM(q);
-    pR = p*R;
-end
-
--- liveView/updateSonar.m
+++
@@ -1,5 +0,0 @@
-function updateSonar(X, handles)
-    axes(handles(1));
-    cla(handles(1));
-    plot([0 1], [X(3) X(3)]);
-end
